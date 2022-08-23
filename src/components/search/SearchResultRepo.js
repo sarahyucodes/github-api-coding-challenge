8eec03298639cb51cc3f75c23c5b2ec2a355ddbc
Sarah Yu <sarahyu.work@gmail.com>
2022-08-23 13:30:19 -0400

    wip

--- src/components/search/SearchResultRepo.js
+++ src/components/search/SearchResultRepo.js
@@ -10,8 +10,10 @@ export const SearchResultRepo = ({ repoId }) => {
   const repo = useSelector(state => selectResultById(state, repoId))
   const favoriteIds = useSelector(selectFavoriteIds)
 
+  const repoExistsInFavorites = favoriteIds.indexOf(repoId.toString()) > -1
+
   const handleSelect = () => {
-    if (favoriteIds.indexOf(repoId.toString()) > -1) {
+    if (repoExistsInFavorites) {
       console.log('You already saved this repo.')
       return
     }
@@ -35,20 +37,20 @@ export const SearchResultRepo = ({ repoId }) => {
 
   return  (
     <li
-      onClick={handleSelect}
-      className='my-1 last-of-type:mb-0 p-2 cursor-pointer hover:bg-gray-100 rounded-lg'
+      tabIndex={0}
+      className='grid grid-cols-12 gap-x-2 items-center my-1 last-of-type:mb-0 p-2 rounded-lg hover:bg-gray-100 focus:bg-gray-100'
     >
-      <div className='grid grid-cols-12 gap-x-2 items-center'>
-        <span className='col-span-8 mr-4 flex items-center'>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+      <span className='col-span-6'>{repo.full_name}</span>
+      <span className='col-span-2 justify-self-end'>{repo.language}</span>
+      <span className='col-span-2 justify-self-end'>{repo.stargazers_count} Stars</span>
+      <span className='col-span-2 justify-self-end'>
+        <button onClick={handleSelect} disabled={repoExistsInFavorites} aria-label={`Save repo`} title={repoExistsInFavorites ? 'Already saved' : 'Save'}>
+          <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 ${repoExistsInFavorites ? 'text-red-600'  : 'text-gray-400'}`} viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
           </svg>
-          <span className='ml-2'>{repo.full_name}</span>
-        </span>
-        <span className='col-span-2 text-xs justify-self-end'>{repo.language}</span>
-        <span className='col-span-2 text-xs justify-self-end'>{repo.stargazers_count} Stars</span>
-      </div>
-      <div className='text-xs pt-2 pl-2 ml-5'>{repo.description}</div>
+        </button>
+      </span>
+      <div className='col-span-6'>{repo.description}</div>
     </li>
   )
 }
